356b9219f64590a6c0ef2f5ed85f7279e1f0a2bd
Xelebee <79708786+xVictorique@users.noreply.github.com>
2021-06-17 00:49:32 +0800

    1.1.100

--- app/gui.py
+++ app/gui.py
@@ -21,35 +21,30 @@ def function():
 
 root = Tk()
 root.title("Lazuli Cross")
-root.geometry("400x500")
+root.geometry("300x400")
 root.iconbitmap(str(pl.Path(__file__).parent) + "/logo.ico")
 
-
-f1 = Frame(root, height=400, width=500)
+f1 = Frame(root, height=400, width=300)
 f1.config(background='#32454a')
 
+f1.pack(fill = 'both', expand = True)
 
-
-f2 = Frame(f1)
-f2.config()
-
+f2 = Frame(f1, height = 300, width = 400)
+f2.grid()
 
 lbl = Label(f2, padx = 100, pady = 100, bd = 1)
+lbl.pack()
+
+f2.pack(expand = True)
 
 f3 = Frame(f1)
 f3.config(background = '#32454a')
+f3.pack(expand = True)
 
 
 b1 = Button(f3, text="Insert Image", bg = '#5a7982', fg = 'white', command = image)
-b1.grid(row=0, column=0, padx= 10, pady=10)
+b1.grid(row=0, column=1, padx= 10, pady=10)
 
 b2 = Button(f3, text = "Test Image", bg = '#5a7982', fg = 'white', command = function)
-b2.grid(row=0, column=1, padx=10, pady=10)
+b2.grid(row=0, column=2, padx=10, pady=10)
 
-
-lbl.pack()
-f1.pack(fill='both',expand=True)
-
-f2.pack(expand=True)
-
-f3.pack(expand=True)
